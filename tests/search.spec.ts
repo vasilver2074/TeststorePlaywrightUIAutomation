24612b874328a267957ae0b547b0b06177baad5d
Valerii Silvanovych <vasilver@gmail.com>
2026-01-13 14:24:23 +0200

    fix Ui + API search test

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -11,7 +11,7 @@ test.describe("Teststore UI Playwright automation Search functionality", () => {
     },
     async ({ mainPage }) => {
       //expect(await mainPage.isSignOutVisible()).toBe(true);
-      await mainPage.isSignOutVisible()
+      await mainPage.isSignOutVisible();
     }
   );
 
@@ -22,38 +22,41 @@ test.describe("Teststore UI Playwright automation Search functionality", () => {
     },
     async ({ mainPage, searchResultPage, request }) => {
       await mainPage.inputSearchMessage("accessories");
-      // await mainPage.runSearch();
+      await mainPage.runSearch();
+
+      expect(await searchResultPage.isSearchResultsVisible()).toBe(true);
 
       const response = await request.get(
-        "https://teststore.automationtesting.co.uk/index.php?action=getAllWishlist&fc=module&module=blockwishlist&controller=action",
+        "https://teststore.automationtesting.co.uk/index.php?controller=search&s=accessories",
         {
           headers: {
-            accept: "*/*",
+            accept:
+              "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
             "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
-            priority: "u=1, i",
+            priority: "u=0, i",
             "sec-ch-ua":
               '"Google Chrome";v="143", "Chromium";v="143", "Not A(Brand";v="24"',
             "sec-ch-ua-mobile": "?0",
             "sec-ch-ua-platform": '"Windows"',
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
             "sec-fetch-site": "same-origin",
+            "sec-fetch-user": "?1",
+            "upgrade-insecure-requests": "1",
             cookie:
-              "PHPSESSID=e7e977561e4732e60491f687b0b2c2bc; ajs_anonymous_id=d037128a-5c44-4cb9-bc76-47123095c09c; PrestaShop-bd73d297b14c5070734013be8110710b=def50200b22792c055fdc0d773edc07c683db2de75ca711c047d77427cab7ddaf79bf09eed1b5822cf9d529c72380949f9c97565f4b71440a6a7e9d6b5953900ec3612747cb44a9daba2dd29538ba9b5c3d2098fc674d48f9d6651bb98ae204daf584adff2ea225ac87587421b618d90c2a663c361e4fc469d042674e38c34668492fd91863de6a7bcb1544d2860dad5e8855ced4d5111bb4eb4bccfcbadba2988eeb16b3066bb5c3398e40d6d7704f4916f0b41f6927ace44c5b9ad2fd66daae31bb9aacde8122ef5cffb2b25a5f1c52c5e4c6ff78c45b611c1346763960d512dc6f0b088e9173dc5168a749441d9980a400fe1db89f9eab39ca1fdf9b9340d33dcafd6e05474b8e75b8808c7a86a5535516e8ebbaf0762ce84332736f779008b43cad3ce262d156de33ffb8c6db5e33fcfd85845bb2bf5f653d8470f5c5b863bc4ba16ebc3fb78c4a384ee13d2c5bfaf21d2fe9ab849e32ee5957dd4849a261ef9ffb48325fbee2d02f138ae9709824b11b5cce5076235f6e52b4a4497ef4d33c04082f7634f9e9b087efff15be87b02469dc61bf2423cc177edd054d1bfba2c26135a310d09cba1ece8e3a526f20de1cc1c7f4360dab078e3432b24c2ea06d8488b691afcce51b12e9bfb842fc5053be3ccee4c78637d46032425eaa7af136167eb8d1a1f1101d656fd87ecdedc4451c9669fa3cf23b3197a42c993288ffd2c8f7d398eebb6150c3f20f5e56921532adf7307eff638a15f78a3c3393b9e13465ee50c0b0eda7cce85c7620de469",
-            Referer:
-              "https://teststore.automationtesting.co.uk/index.php?controller=search&s=accessories", 
+              "PHPSESSID=4ab215bad06be12ba0afdaab6f95dac5; ajs_anonymous_id=74421ec6-722b-46be-b94a-a99eae08b149; PrestaShop-bd73d297b14c5070734013be8110710b=def50200ebd9a16f04a077287f86d7038018848663479b0ead91e2df2c00ea1d04603c712fcc11a8521678cf791bb7d456bb28e940837d226505b63319290628e8c74a5307f3827f2f2dc4fc78b3a90a26b11eed8d848def0c4755dbbd34a9a5cea531e4ae776d4a9d095301d7d8459251b6d1876a3116f12abadd4efeaaa8e797d11a16bee00b1d36cd72348dfe90c1ac849b4b2375b83670e15956d4452f9d5867d2191bc25b4bb2d855e921ff076bdbc21692f18e5a402fa8162841d1094f2890d575d4f56762746d6b3d6d14bffd3ff03ba0d7b4218519ae1a6a918bd99b3a093bf1390e32646d574ae922c83d666e6c7785a35e2e6bf782b48732aa1c10cddc82fc145f24d0d29ddd740386e99027acc4240af0183211908e75c1a1eca064b5dc574e878b323e5568e647d0157a30297f7045fa38706ef4005a3d01ecb9b19e3056990588a629fbc0ad5f53fbe56486935480e1d1fb2f173283b54c255f6cb9aa69152207a2b77b3c35e778d21ca5f8b91b8d465cde7f6a7bb375e8f36d6d04cdd6f023a8ee92b9f2cfae7ddbde4c12727f5b71f261888e83742d2693f437f9565798892519743996730cd1ac6cdc7cee72b7776134df0fe285087b2493b18aea974f7033a9b26a96566fda07d171e1cdbc20d4cd991cc4eafe53f8221c33d927550f4f378ebb64b7fe4e6caa2c8cbfcc166fc2722252e65740174874a2e37800bbbbb513b020b3e8ad94db3682a9e244d30ff8f6da80a72bd40b72caff0ec656aef292dddd4e16",
+            Referer: "https://teststore.automationtesting.co.uk/index.php",
           },
           failOnStatusCode: true,
         }
       );
       //console.log(await response.text());
-      
-      const responseBody = await response.json();
+      //const responseBody = await response.json();
       //const token = responseBody.wishlists[0].token;
-
       //expect(token).toBeTruthy();
 
-      expect(await searchResultPage.isSearchResultsVisible()).toBe(true);
+      expect(response.status()).toBe(200);
+      expect(await response.text()).toContain("Accessories");
     }
   );
 
@@ -82,21 +85,3 @@ test.describe("Teststore UI Playwright automation Search functionality", () => {
     }
   );
 });
-
-// fetch("https://teststore.automationtesting.co.uk/index.php?action=getAllWishlist&fc=module&module=blockwishlist&controller=action", {
-//   "headers": {
-//     "accept": "*/*",
-//     "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
-//     "priority": "u=1, i",
-//     "sec-ch-ua": "\"Google Chrome\";v=\"143\", \"Chromium\";v=\"143\", \"Not A(Brand\";v=\"24\"",
-//     "sec-ch-ua-mobile": "?0",
-//     "sec-ch-ua-platform": "\"Windows\"",
-//     "sec-fetch-dest": "empty",
-//     "sec-fetch-mode": "cors",
-//     "sec-fetch-site": "same-origin",
-//     "cookie": "PHPSESSID=e7e977561e4732e60491f687b0b2c2bc; ajs_anonymous_id=8f933f3e-77ff-4268-b52c-b34832844d5c; PrestaShop-bd73d297b14c5070734013be8110710b=def502004b559e2c8590ff56221b4523b05d84837a8c9eef6333b53574954568c298887e4b50a330b8cf62ad878557214ee4ee96ee6cbdcc126025bca0c7e5c922b924749fa5f170c5ab15d7cbf5aafc42c4de1c14d3423af3fe6f5423753fd94047f07e1488bf97eeb63008868492b9e004649ff839abf0ff99db8695f81135038aae44c189b042966973c2a799cdd1423f6809c0d47cd979f1db554428f29ab437dc492b4f50ba6c146bb72be2590d45127d984ecd6bb6240739039e315b2ede5ab6a605cda5ee59c9d539d715e1058e944478d34edc0e815cf10ddd25bbfd1aedbe49bf7cd3e795c88855e426d89b8bf1bcf40f3f77da5692016db05089ed6b9412c5cd7e0b9e9ae2db36842722577e8acd6506c65c2a119dd4aa42955224d770ed11c87a2ec3d36ed782b51745c6965e9672234909fa4b8bfdf2effced23363779131c836575754a9bb9e8709a5c3ce48e054b86e4f738562d8578234f6f7545eabf2ae21f18bd7f7bed806ad294482149940d71e1e98e8f5d8af30684a60da12e730a8e180393d94419c68a20d2803a775de600cdc1042e819deb91af3c8869cac3b1b877816028b7e3603ee27219e1c8dcd13bc320e5261f47272746db82dd845dd04770b40a20a1a41c0f8d00d77f33a6f7d9a859d3f3c12d8ee977c5af6fba30b98a5f8671272d8241aa39bf01716f12b76dba62dbb6c72915b96556e885ce5a802b863ef8bdf8259bdf81199fb55ca2b96a0b7c620b4483f9a0345821e7370912c0fa0c7dae13fe604589",
-//     "Referer": "https://teststore.automationtesting.co.uk/index.php?controller=search&s=accessories"
-//   },
-//   "body": null,
-//   "method": "GET"
-// });
